5c97d72d23fd48aa314f9b12c442e8e6cf14ded6
Niegil-Francis <niegilfrancis14@gmail.com>
2021-10-15 20:30:02 +0530

    Added include and msg folders

--- build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = '/home/niegil/dobot_ws/src'
-whitelisted_packages = ''.split(';') if '' != '' else []
-blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/home/niegil/cobot_ws/devel;/opt/ros/noetic'.split(';') if '/home/niegil/cobot_ws/devel;/opt/ros/noetic' != '' else []
